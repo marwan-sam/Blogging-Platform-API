1195f10869fc892abdd2929f91e2109d6050d00f
marwan-sam <enzo.meme11111@gmail.com>
2024-12-21 10:41:46 +0200

    Create Test Projects

---
+++ tests/BloggingPlatform.Auth.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace BloggingPlatform.Auth.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/BloggingPlatform.Services.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace BloggingPlatform.Services.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
